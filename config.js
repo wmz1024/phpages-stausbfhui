18d7bfb043af77ec7efc6f74f2ef8291b9810bb4
mz w <82153828+wmz1024@users.noreply.github.com>
2021-05-24 20:00:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm788207926-0ef34814ffcb78ccccb62075',
   ],
 
   // 是否显示监测站点的链接
@@ -28,7 +25,7 @@ window.Config = {
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 40,
 
   // 导航栏菜单
   Navi: [
